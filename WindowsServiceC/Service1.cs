0d7f958bcf7b7b8d7ad8857f29776507f1b61eb9
r.devika@igenius.in <r.devika@igenius.in>
2024-08-29 11:21:40 +0530

    Add project files.

---
+++ WindowsServiceC/Service1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsServiceC
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            string logFilePath = @"C:\Users\LENOVO\source\repos\WindowsServiceC\EntryFile.txt";
+            Directory.CreateDirectory(Path.GetDirectoryName(logFilePath));
+            File.AppendAllText(logFilePath, $"Service started at {DateTime.Now}\n");
+        }
+
+        protected override void OnStop()
+        {
+            string logFilePath = @"C:\Users\LENOVO\source\repos\WindowsServiceC\EntryFile.txt";
+            File.AppendAllText(logFilePath, $"Service stopped at {DateTime.Now}\n");
+        }
+    }
+}
